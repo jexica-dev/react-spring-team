3981182592b7aa00d2d5b194569e20d093714a90
Rayvon Gregory <rayvon.gregory@genspark.net>
2022-06-16 19:13:57 -0400

    game card btns

--- client/src/Components/Game/GameCard.jsx
+++ client/src/Components/Game/GameCard.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function GameCard(props) {
 
+
+  const addToLikedGames = (e) => {
+    const { target } = e
+    const gameId = target.getAttribute("game_id")
+    const userId = props.userId
+    axios.post("/liked-games", { userId, gameId }).then(res => {
+      console.log("the game was added to this user's liked list")
+    }).catch(err => {
+      console.log("nah breh")
+    })
+
+  }
+
+  const addToDisLikedGames = (e) => {
+    const { target } = e
+    const gameId = target.getAttribute("game_id")
+    const userId = props.userId
+    axios.post("/disliked-games", { userId, gameId }).then(res => {
+      console.log("the game was added to this user's liked list")
+    }).catch(err => {
+      console.log("nah breh")
+    })
+
+  }
 
 
 
   return (
     <>
       <div className="grid place-content-center">
-      <Link className="card" to={`/games/${props.id}`}>
+      <Link className="card" to={`/games/${props.gameId}`}>
         <h4 className="text-center">{props.name}</h4>
       </Link>
         <img className="object-cover h-48 min-w-full" src={props.background_image} alt={props.name} />
@@ -18,8 +43,8 @@ export default function GameCard(props) {
           <p>Released:{props.released}</p>
         </div>
         <div className="flex">
-          <button className="grow whitespace-nowrap font-medium tracking-wider py-3 px-10 hover:bg-trp bg-black text-white">👍</button>
-          <button className="grow whitespace-nowrap font-medium tracking-wider py-3 px-10 hover:bg-trp bg-black text-white">👎</button>
+          <button onClick={addToLikedGames} game_id={props.gameId} className="grow whitespace-nowrap font-medium tracking-wider py-3 px-10 hover:bg-trp bg-black text-white">👍</button>
+          <button onClick={addToDisLikedGames} game_id={props.gameId} className="grow whitespace-nowrap font-medium tracking-wider py-3 px-10 hover:bg-trp bg-black text-white">👎</button>
         </div>
       </div>
       </>
